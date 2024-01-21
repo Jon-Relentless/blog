07cdac82f512121045f7f804b1ccd90c2590d973
Jon-Relentless <JonLR@protonmail.com>
2024-01-21 14:46:11 -0500

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root "articles#index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+  resources :articles
+end
